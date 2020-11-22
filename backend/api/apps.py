2f6e4e5a751b24dd4acc3d8cc802bf2435f052c7
Fatma Tanrisevdi <fatma@armut.com>
2020-11-22 21:14:10 +0300

    first django api was created

---
+++ backend/api/apps.py
@@ -0,0 +1,9 @@
+from django.apps import AppConfig
+
+
+class Swe573ApiConfig(AppConfig):
+    name = 'api'
+
+
+class TwitterAuthConfig(AppConfig):
+    name = 'twitter_auth'
--- src/first-of-many.py
+++ src/first-of-many.py
@@ -14,42 +14,14 @@ auth.set_access_token(os.environ.get("PPP"),
                       os.environ.get("LLL"))
 
 api = tweepy.API(auth)
-
-public_tweets = api.search('starwars')
+user = api.get_user('twitter')
+public_tweets = api.search('reis')
 x = []
 for tweet in public_tweets:
-    x.append({'twits': tweet.text})
-
-print(x)
-df = pd.read_json(x, encoding="latin-1")
-
-comment_words = ''
-stopwords = set(STOPWORDS)
-
-# iterate through the csv file
-for val in df.CONTENT:
-
-    # typecaste each val to string
-    val = str(val)
-
-    # split the value
-    tokens = val.split()
-
-    # Converts each token into lowercase
-    for i in range(len(tokens)):
-        tokens[i] = tokens[i].lower()
-
-    comment_words += " ".join(tokens)+" "
-
-wordcloud = WordCloud(width=800, height=800,
-                      background_color='white',
-                      stopwords=stopwords,
-                      min_font_size=10).generate(comment_words)
-
-# plot the WordCloud image
-plt.figure(figsize=(8, 8), facecolor=None)
-plt.imshow(wordcloud)
-plt.axis("off")
-plt.tight_layout(pad=0)
-
-plt.show()
+    x.append({'id':  tweet.id, 'createdAt': tweet.created_at,
+              'text': tweet.text,
+              'user': tweet.user,
+              'entities': tweet.entities})
+print('----------------')
+print(x[0])
+print('----------------')
