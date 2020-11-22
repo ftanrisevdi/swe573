2157be41e20d11f5fe2b5c2f46059a3b374e4913
Fatma Tanrisevdi <fatma@armut.com>
2020-11-22 22:19:42 +0300

    no message

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+c4q)y+vcl6hq#&l+jc+m13#@cmq6bd0!t$6s*n%3kt%*dgo02'
+SECRET_KEY = os.environ.get("DjangoSecretKey")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
