f5370953d3d64b643c366ecb25909f71dd8d5546
Fatma Tanrisevdi <fatma@armut.com>
2021-02-01 19:03:44 +0300

    mocks are changed

--- rest/rest/settings.py
+++ rest/rest/settings.py
@@ -60,9 +60,10 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
-CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:4200",
+    "https://swe573-client.herokuapp.com/"
 ]
 
 ROOT_URLCONF = 'rest.urls'
